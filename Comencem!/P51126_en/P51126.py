db289f4903193145ff4f17591efa20057f44d006
Fks32 <falseacc004@gmail.com>
2024-11-26 23:11:58 +0100

    P51126

--- Comencem!/P51126_en/P51126.py
+++ Comencem!/P51126_en/P51126.py
@@ -3,3 +3,12 @@ a1 = read(int)
 b1 = read(int)
 a2 = read(int)
 b2 = read(int)
+
+if b1 < a2:
+    print("[]")
+elif a1 > a2 :
+    if b1 > b2:
+        print("[",a1,",",b2,"]", sep="")
+    else:
+        print("[",a1,",",b1,"]", sep="")
+        
