dfbafbb40776f4e82bdcc8ccfcea4e072c920826
CNT06fg <fg0613528@gmail.com>
2025-06-27 17:48:33 +0200

    s

--- Bucles/P37500_ca_Primers_nombres.py
+++ Bucles/P37500_ca_Primers_nombres.py
@@ -1,11 +1,13 @@
-// Primers nombres
-// https://jutge.org/problems/P37500_ca
-// P37500_ca:std:none:
-// Created on 6/27/2025, 5:42:06 PM by 徐渭
+# Primers nombres
+# https://jutge.org/problems/P37500_ca
+# P37500_ca:std:none:
+# Created on 6/27/2025, 5:42:06 PM by 徐渭
 
-#include <iostream>
-using namespace std;
+import yogi
 
-int main() {
+n = yogi.read(int)
+i = 0
+while (i <= n):
+    print(i, end='\n')
+    i += 1
 
-}
